b0ce8e0bc07459a65e5c6bdd339502c092137965
Herick2D <contato.herickmoreira@gmail.com>
2026-05-20 12:57:47 -0300

    feat: add GET students

--- backend/src/modules/students/students-controller.js
+++ backend/src/modules/students/students-controller.js
@@ -2,8 +2,9 @@ const asyncHandler = require("express-async-handler");
 const { getAllStudents, addNewStudent, getStudentDetail, setStudentStatus, updateStudent } = require("./students-service");
 
 const handleGetAllStudents = asyncHandler(async (req, res) => {
-    //write your code
-
+    const { name, class: className, section, roll } = req.query;
+    const students = await getAllStudents({ name, className, section, roll });
+    res.status(200).json({ students });
 });
 
 const handleAddStudent = asyncHandler(async (req, res) => {
